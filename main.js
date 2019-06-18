cfd61db4006ce0bddf9ff80533f2d568b2df6356
richu <richursa@gmail.com>
2019-06-18 18:42:01 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var message = "hello world";
+console.log(message);
+var isBoolean = false;
+var isNumber = 3;
---
+++ main.ts
@@ -0,0 +1,7 @@
+export {}
+let message: string = "hello world";
+console.log(message);
+let isBoolean: boolean = false
+let isNumber:number =3
+let list1:number[] = [1,2,3,4,5]
+let list2:Array<number>= [1,2,3]
