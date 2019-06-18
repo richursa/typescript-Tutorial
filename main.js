9f305c12256f88e2d68523b158e7e8ed8b814c4f
richu <richursa@gmail.com>
2019-06-18 18:46:17 +0530

    trying enums and type specification

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ var message = "hello world";
 console.log(message);
 var isBoolean = false;
 var isNumber = 3;
+var list1 = [1, 2, 3, 4, 5];
+var list2 = [1, 2, 3];
+var person = ['richu', 5];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Green;
+console.log(c);
--- main.ts
+++ main.ts
@@ -5,3 +5,7 @@ let isBoolean: boolean = false
 let isNumber:number =3
 let list1:number[] = [1,2,3,4,5]
 let list2:Array<number>= [1,2,3]
+let person:[string , number] = ['richu',5]
+enum Color{Red , Green , Blue}
+let c: Color = Color.Green
+console.log(c);
